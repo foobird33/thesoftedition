0b6ba7bfb8e7c1aed9bd18550077c585885853ac
foobird33 <foobird33@gmail.com>
2026-01-16 13:08:07 +0800

    Delete my portfolio directory

--- my portfolio/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-// Simple script to update the copyright year automatically
-document.getElementById('current-year').textContent = new Date().getFullYear();
-
-// You can add more simple interactivity here later
-console.log("Portfolio site loaded!");
