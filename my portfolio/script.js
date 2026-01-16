e9c75e15555118c00ac3263c433e18be20ccab44
foobird33 <foobird33@gmail.com>
2026-01-16 13:02:24 +0800

    Add files via upload

---
+++ my portfolio/script.js
@@ -0,0 +1,6 @@
+// script.js
+// Simple script to update the copyright year automatically
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+// You can add more simple interactivity here later
+console.log("Portfolio site loaded!");
